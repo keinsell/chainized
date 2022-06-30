a3b278bf9555b02cbb86e2326b2e2f5d77aea93a
Jay <jakub.jay.olan@gmail.com>
2022-06-30 11:01:30 +0000

    chore: :hammer: import `SOLANA_KEYPAIR`

--- contracts/SPL/scripts/postinstall.mjs
+++ contracts/SPL/scripts/postinstall.mjs
@@ -8,7 +8,7 @@ $.verbose = false;
 await $`solana config set --url ${env.SOLANA_NETWORK} -k ${env.SOLANA_ID}`;
 echo(`Configured Solana CLI: ${env.SOLANA_CONFIG}`);
 
-await $`echo ${env.SOLANA_KEYPAIR} > ${env.SOLANA_ID}`;
+await fs.outputJson(env.SOLANA_ID, env.SOLANA_KEYPAIR);
 
 try {
   await $`solana-keygen new -o ${env.SOLANA_ID} -s --no-bip39-passphrase`;
