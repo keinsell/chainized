ff753d21125d636b8a149ab0f08b9dc9fb09c434
Jay <jakub.jay.olan@gmail.com>
2022-06-30 13:24:23 +0000

    chore: :wrench: do not use `devDefault`, use `default` instead

--- packages/config/index.ts
+++ packages/config/index.ts
@@ -9,17 +9,16 @@ const env = cleanEnv(process.env, {
     choices: ["development", "production"],
     default: "development",
   }),
-  EVM_NETWORK: str({ devDefault: "rinkeby" }),
+  EVM_NETWORK: str({ default: "rinkeby" }),
   EVM_PRIVATE_KEY: str({
-    devDefault:
-      "2ff5e84778923134c187b071c53c2f405b38748997706cc23bdd6b63411692c4",
+    default: "2ff5e84778923134c187b071c53c2f405b38748997706cc23bdd6b63411692c4",
   }),
   /** `EVM_ADDRESS` stands for public address on EVM-based networks. */
   EVM_ADDRESS: str({
-    devDefault: "0xC5702332b89000aA73ea8F497C888aDcf0829A85",
+    default: "0xC5702332b89000aA73ea8F497C888aDcf0829A85",
   }),
   SOLANA_KEYPAIR: json({
-    devDefault: [
+    default: [
       180, 225, 110, 224, 110, 75, 66, 169, 107, 169, 22, 255, 127, 220, 5, 145,
       106, 23, 74, 247, 247, 46, 149, 51, 76, 206, 136, 81, 38, 112, 212, 164,
       243, 40, 232, 116, 66, 70, 45, 87, 22, 148, 26, 35, 133, 23, 202, 199, 72,
@@ -30,7 +29,7 @@ const env = cleanEnv(process.env, {
     default: `${process.env["HOME"]}/.config/solana/id.json`,
   }),
   SOLANA_NETWORK: str({
-    devDefault: `localhost`,
+    default: `localhost`,
   }),
   SOLANA_CONFIG: str({
     default: `${process.env["HOME"]}/.config/solana/cli/config.yml`,
